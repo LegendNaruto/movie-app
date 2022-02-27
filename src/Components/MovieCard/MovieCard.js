3bb099d2497be6ca165741d1444215f2940dbdbb
Jassem Baccari <jassembaccari0000@gmail.com>
2022-02-27 22:44:58 +0100

    movie app

---
+++ src/Components/MovieCard/MovieCard.js
@@ -0,0 +1,21 @@
+/* eslint-disable jsx-a11y/alt-text */
+
+import "./moviecard.css";
+
+export default function MovieCard({ele}) {
+    return (
+        <div className="MovieCard">
+                <div>
+                    <div>
+                    <img width="300" src={ele.img} />
+                    </div>
+                    <div >
+                    <h2>{ele.title}</h2>
+                    <p>{ele.description}</p>
+                    <h3>Rate : {ele.rating}</h3>
+                    <h4>{ele.posterURL}</h4>
+                    </div>
+                </div>
+            </div>
+    )
+}
